3b74f567e7adfff7b9e9ad7fea20628b318edeab
SamBat2005 <batcsg1@student.op.ac.nz>
2025-05-11 13:43:39 +1200

    refactor: removed ISR logic for menu states

--- Project/Project.ino
+++ Project/Project.ino
@@ -10,7 +10,6 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);  // I2C address 0x27, 16 column and 2 rows
 int dryValue = 330;
 int wetValue = 650;
 int percentage;
-
 int moisture;
 
 //Temp and humidity values
@@ -18,38 +17,26 @@ int temperature;
 int humidity;
 
 ///Buzzer
-//int buzzer = 13;
+int buzzer = 13;
 
 //Buttons
 int tempH = 2;
 int moistureBtn = 3;
 
-volatile bool home = true;
-//Button ISR booleans
-volatile bool tempHMenu = false;
-volatile bool moistureMenu = false;
-
-void tempHISR() {
-  tempHMenu = true;
-  moistureMenu = false;
-}
-
-void moistureISR() {
-  moistureMenu = true;
-  tempHMenu = false;
-}
+//Menu state enum
+enum MenuState { HOME,
+                 TEMP_HUMIDITY,
+                 MOISTURE };
+MenuState currentMenu = HOME;
 
 void setup() {
   // Initialize serial communication to allow debugging and data readout.
   // Using a baud rate of 9600 bps.
   Serial.begin(9600);
 
-  //pinMode(buzzer, OUTPUT);
+  pinMode(buzzer, OUTPUT);
   pinMode(tempH, INPUT_PULLUP);
   pinMode(moistureBtn, INPUT_PULLUP);
-
-  attachInterrupt(digitalPinToInterrupt(tempH), tempHISR, FALLING);
-  attachInterrupt(digitalPinToInterrupt(moistureBtn), moistureISR, FALLING);
 
   //Declare data headers
   Serial.print("Temperature(C), ");
@@ -73,29 +60,26 @@ void loop() {
   Serial.print(" ");
   Serial.print(percentage);  //Moisture
   Serial.print(" ");
-  // Serial.print(moisture);  //Moisture
-  // Serial.print(" ");
   Serial.println();
-  //Where the LCD code lives
-  stateHandler();
+
+  checkButtons();
   delay(1000);
 }
 
-void menu() {    
-  lcd.clear();           // clear display
-  lcd.setCursor(0, 0);         // move cursor to   (0, 0)
-  lcd.print("PlantMate");  // print message at (0, 0)
-  lcd.setCursor(0, 1);         // move cursor to   (2, 1)
-  lcd.print("Smart Irrigation");     // print message at (2, 1)
+void beep() {
+  tone(buzzer, 1000);  // Send 1KHz sound signal...
+  delay(1000);         // ...for 1 sec
+  noTone(buzzer);      // Stop sound...
+  delay(1000);         // ...for 1sec
 }
 
-//Logic functions for Temprature, Humidity and Soil Moisture
+//-- Logic functions for Temprature, Humidity and Soil Moisture
 void soilMoisture() {
   moisture = analogRead(moisturePin);
   percentage = map(moisture, dryValue, wetValue, 0, 100);
   //ChatGPT code for keeping percentage within 0 to 100%
   //Prompt: how to keep a percentage range from 0 to 100% within range
-  percentage = constrain(percentage, 0, 100); //Constrain percentage within 0 to 100%
+  percentage = constrain(percentage, 0, 100);  //Constrain percentage within 0 to 100%
 }
 
 void tempHumidity() {
@@ -103,7 +87,15 @@ void tempHumidity() {
   int result = dht11.readTemperatureHumidity(temperature, humidity);
 }
 
-//Print functions for Temprature, Humidity and Soil Moisture
+void menu() {
+  lcd.clear();                    // clear display
+  lcd.setCursor(0, 0);            // move cursor to   (0, 0)
+  lcd.print("PlantMate");         // print message at (0, 0)
+  lcd.setCursor(0, 1);            // move cursor to   (2, 1)
+  lcd.print("Smart Irrigation");  // print message at (2, 1)
+}
+
+//-- Print functions for Temprature, Humidity and Soil Moisture
 void printTH() {
   //LCD Code will go here
   lcd.clear();  // clear display
@@ -118,31 +110,37 @@ void printMoisture() {
   lcd.clear();  // clear display
   lcd.setCursor(0, 0);
   lcd.print("Moisture: " + String(percentage) + "%");
-  if (percentage < 20){
+  if (percentage < 20) {
     lcd.setCursor(0, 1);
     lcd.print("Plant is sad:(");
-  }else if (percentage > 20 && percentage < 70){
+    beep();
+  } else if (percentage > 20 && percentage < 70) {
     lcd.setCursor(0, 1);
     lcd.print("Plant is happy:)");
-  }else if (percentage > 70){
+  } else if (percentage > 70) {
     lcd.setCursor(0, 1);
     lcd.print("Plant is wet:)");
   }
 }
 
-void stateHandler() {
-  if (home){
-    menu();
+void checkButtons() {
+  delay(50);
+  if (digitalRead(tempH) == LOW) {
+    currentMenu = TEMP_HUMIDITY;
   }
-  if (tempHMenu) {
-    home = false;
-    moistureMenu = false;
-    Serial.println("Temperature and humidity button pressed");
-    printTH();     // Show temperature and humidity
-  } else if (moistureMenu) {
-    home = false;
-    tempHMenu = false;
-    Serial.println("Soil moisture button pressed");
-    printMoisture();  // Show soil moisture only
+
+  if (digitalRead(moistureBtn) == LOW) {
+    currentMenu = MOISTURE;
+  }
+  menuState();
+}
+
+void menuState() {
+  if (currentMenu == HOME) {
+    menu();
+  } else if (currentMenu == TEMP_HUMIDITY) {
+    printTH();
+  } else if (currentMenu == MOISTURE) {
+    printMoisture();
   }
 }
